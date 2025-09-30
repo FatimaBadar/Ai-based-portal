23d3945759a819ba4d3fdd2c4df426d6c1dc2d4c
Noor Fatima Qureshi <128814375+FatimaBadar@users.noreply.github.com>
2025-09-30 21:42:10 +1300

    API URL changed

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = import.meta.env.VITE_SERVER_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
